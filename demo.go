a82cee2a4950b543a0406fad6379a27799c9b2e6
Johnny <zhaozy93@outlook.com>
2018-10-20 16:23:30 +0800

    Update demo.go

--- demo.go
+++ demo.go
@@ -1,4 +1,4 @@
-package geohash_go
+package main
 
 import (
 	"fmt"
@@ -6,10 +6,25 @@ import (
 )
 
 func main() {
-	hashKey, _ := geohash_go.EnGeoHash(float64(39.928167), float64(116.389550), 50)
+	tBeg := time.Now().UnixNano()
+	hashKey, _ := geohash_go.EnGeoHash(float64(39.928167), float64(116.389550), 10)
+	// hashKey, _ := geohash_go.EnGeoHash(float64(53.170996), float64(3.69526), 10)
+	// hashKey, _ := geohash_go.EnGeoHash(float64(39.990234375), float64(116.5429687), 10)
+	fmt.Println("EnGeoHash Time used:", (time.Now().UnixNano()-tBeg)/1000)
 	fmt.Println(hashKey)
+
+	tBeg = time.Now().UnixNano()
 	lat, lng, _ := geohash_go.DeGeoHash(hashKey)
+	fmt.Println("DeGeoHash Time used:", (time.Now().UnixNano()-tBeg)/1000)
 	fmt.Println(lat, lng)
-	neig, _ := geohash_go.GetNeighour(hashKey)
-	fmt.Println(neig)
+
+	tBeg = time.Now().UnixNano()
+	neigh, _ := geohash_go.GetNeighour_back(hashKey)
+	fmt.Println("GetNeighour_back Time used:", (time.Now().UnixNano()-tBeg)/1000)
+	fmt.Println(neigh)
+
+	tBeg = time.Now().UnixNano()
+	neigh, _ = geohash_go.GetNeighour(hashKey)
+	fmt.Println("GetNeighour Time used:", (time.Now().UnixNano()-tBeg)/1000)
+	fmt.Println(neigh)
 }
